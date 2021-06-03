76aba32958532732be35c7314c4b88b7855e6c31
Harshit Sahu <57841653+harshsahu766@users.noreply.github.com>
2021-06-03 10:04:30 +0530

    Create file_handling.c

---
+++ file_handling.c
@@ -0,0 +1,49 @@
+[10:03 AM, 6/3/2021] Harsh: #	include <stdio.h>
+
+#	include <string.h>
+
+int main( )
+
+{
+
+FILE *filePointer ;
+
+char data[50]
+
+=	"This is sample data"; gets(data);
+
+filePointer = fopen("myfile.txt", "w") ;
+
+if ( filePointer == NULL )
+
+{
+
+printf( "File not found" ) ;
+
+}
+
+else
+
+{
+
+printf("file opened.\n") ;
+
+if ( strlen ( data) > 0 )
+
+{
+
+fputs(data, filePointer) ;
+
+fputs("\n", filePointer) ;
+
+}
+
+fclose(filePointer) ;
+
+
+
+}
+
+return 0;
+
+}
